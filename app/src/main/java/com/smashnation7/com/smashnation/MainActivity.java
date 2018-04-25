e5f23be6ab8ad20b0e093fd10dddc7c894b3631a
atikfahad <fahadatik@gmail.com>
2018-04-25 06:36:35 +0600

    Share button is working, Reload button is also working I guess.. WebView added and successfully getting data from smashnation7.com

--- app/src/main/java/com/smashnation7/com/smashnation/MainActivity.java
+++ app/src/main/java/com/smashnation7/com/smashnation/MainActivity.java
@@ -6,25 +6,47 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        webView = (WebView) findViewById(R.id.smashWeb);
+        webView.loadUrl("https://smashnation7.com");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button reLoad = (Button) findViewById(R.id.bReload);
+        reLoad.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                webView.reload();
+            }
+        });
+        Button share = (Button) findViewById(R.id.bShare);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent i = new Intent(Intent.ACTION_SEND);
+                    i.setType("text/plain");
+                    i.putExtra(Intent.EXTRA_SUBJECT, "Smash Nation");
+                    String sAux = "\nLet Me Recommend You This Application\n\n";
+                    sAux = sAux + "https://play.google.com/store/apps/details?id=the.package.id \n\n";
+                    i.putExtra(Intent.EXTRA_TEXT, sAux);
+                    startActivity(Intent.createChooser(i, "Choose One"));
+                } catch(Exception e) {
+                    //e.toString();
+                }
             }
         });
     }
